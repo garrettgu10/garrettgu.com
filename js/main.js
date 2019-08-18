179455dec45580a8ba67eaaa682a9d442a96b6c3
Garrett Gu <garrettgu777@gmail.com>
2019-08-17 23:30:50 -0500

    add delay to explosion effect

--- js/main.js
+++ js/main.js
@@ -129,6 +129,7 @@ function initializeScrollableElements() {
       rotateX: anime.random(-60, 60),
       rotateZ: rotation,
       opacity: 1,
+      delay: 200,
       duration: anime.random(750, 3000)
     }).finished.then(function() {
       performRotation(shape);
